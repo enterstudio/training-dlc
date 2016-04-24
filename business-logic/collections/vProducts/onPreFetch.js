996a96d4dc542ef52ca1314d99d9fcb70f4ccaa6
amoothart <amooth@gmail.com>
2016-04-24 21:01:03 +0530

    re-init bl

--- business-logic/collections/vProducts/onPreFetch.js
+++
@@ -1,8 +0,0 @@
-function onPreFetch(request, response, modules) {
-	var context = modules.requestContext;
-	//modules.logger.info('major.version: ' + context.clientAppVersion.majorVersion());
-	//modules.logger.info('minor.version: ' + context.clientAppVersion.minorVersion());
-	modules.logger.info('version: ' + context.clientAppVersion.majorVersion() + ' ' + context.clientAppVersion.minorVersion());
-	response.continue();
-	
-}
