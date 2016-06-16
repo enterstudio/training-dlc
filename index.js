850906330c6db176985d5edd6cb36276ce755383
amoothart <amooth@gmail.com>
2016-06-16 15:47:25 -0400

    node paas init for mysql and spotify

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = require('./routes/customersPaas.js');
+module.exports = require('./nodepaas/partner.js');
+//module.exports = require('./nodepaas/artists.js');
+//module.exports = require('./nodepaas/playlists.js');
