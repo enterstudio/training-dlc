ff4af07e7cd123d8ebe47b482ea0508bd48a259e
amoothart <amooth@gmail.com>
2016-10-05 00:19:18 -0400

    add mysql insert

--- nodepaas/partner.js
+++ nodepaas/partner.js
@@ -1,42 +1,51 @@
 var sdk = require('kinvey-backend-sdk');
 var mysql = require('mysql');
+var moment = require('moment');
 const cronjob = require('./cronjob.js');
 
+//TODO: LAB: create the KMR service object
 var service = sdk.service(function(err, service) {
+  //TODO: LAB: create the KMR dataLink object
   var dataLink = service.dataLink;   // gets the datalink object from the service
 
-  setInterval(function(){console.log("periodic ping")},10000);
-  setInterval(cronjob.getAndUpdateData, 60000);
+  setInterval(function(){console.log("periodic ping")},60000);
+  // setInterval(cronjob.getAndUpdateData, 300000);
 
+  //TODO: LAB: create the serviceObject to back your Kinvey collection
   var partner = dataLink.serviceObject('Partner');
 
   if(err != null) {
     console.log(JSON.stringify(err));
   }
 
-  // wire up the event that we want to process
+  //TODO: LAB: wire up the REST endpoints that you need to process
   partner.onGetById(show);
   partner.onGetAll(list);
   partner.onGetCount(count);
+  partner.onInsert(create);
+
+  var connection = mysql.createConnection({
+    host     : 'training-mysql.cbefmmhfdivt.us-east-1.rds.amazonaws.com',
+    user     : 'training',
+    password : 'kinveysql',
+    database : "training"
+  });
 
   function show(req, complete) {
     debugInfo(req);
-    var connection = mysql.createConnection({
-      host     : 'training-mysql.cbefmmhfdivt.us-east-1.rds.amazonaws.com',
-      user     : 'training',
-      password : 'kinveysql',
-      database : "training"
-    });
-
+    //TODO: LAB: run the query you need to get data from your external source
     connection.query('SELECT * FROM partner WHERE id = ' + req.entityId, function(error, rows) {
       if(error == null) {
         console.log("MySQl Response: " + JSON.stringify(rows));
         rows.forEach(function(row) {
+          //TODO: LAB: format the response to match your expected JSON
           row = formatResponse(row);
         })
+        //TODO: LAB: return the correct success response
         return complete(rows).ok().next();
       } else {
         console.log(error);
+        //TODO: LAB: return the correct error response
         return complete(error).runtimeError().next();
       }
     })
@@ -44,13 +53,7 @@ var service = sdk.service(function(err, service) {
 
   function list(req, complete) {
     debugInfo(req);
-    var connection = mysql.createConnection({
-      host     : 'training-mysql.cbefmmhfdivt.us-east-1.rds.amazonaws.com',
-      user     : 'training',
-      password : 'kinveysql',
-      database : "training"
-    });
-
+    //TODO: LAB: run the query you need to get data from your external source
     connection.query('SELECT * FROM partner', function(error, rows) {
       if(error == null) {
         console.log("MySQl Response: " + JSON.stringify(rows));
@@ -66,27 +69,38 @@ var service = sdk.service(function(err, service) {
   }
 
   function count(req, complete) {
-      debugInfo(req);
-      var connection = mysql.createConnection({
-        host     : 'training-mysql.cbefmmhfdivt.us-east-1.rds.amazonaws.com',
-        user     : 'training',
-        password : 'kinveysql',
-        database : "training"
-      });
+    debugInfo(req);
+    //TODO: LAB: run the query you need to get data from your external source
+    connection.query('SELECT COUNT(*) FROM partner', function(error, result) {
+      if(error == null) {
+        console.log("MySQl Response: " + JSON.stringify(result));
+        var response = {"count": result[0]["COUNT(*)"]};
+        return complete(response).ok().next();
+      } else {
+        console.log(error);
+        return complete(error).runtimeError().next();
+      }
+    })
+  }
 
-      connection.query('SELECT COUNT(*) FROM partner', function(error, result) {
-        if(error == null) {
-          console.log("MySQl Response: " + JSON.stringify(result));
-          var response = {"count": result[0]["COUNT(*)"]};
-          return complete(response).ok().next();
-        } else {
-          console.log(error);
-          return complete(error).runtimeError().next();
-        }
-      })
-    }
+  function create(req, complete) {
+    debugInfo(req);
+    //TODO: LAB: run the query you need to get data from your external source
+    var query = 'INSERT INTO partner (Name, Email, created_time, last_modified_time) VALUES ("' + req.body.partnername + '","' + req.body.Email + '","' + moment() + '","' + moment() + '")';
+    console.log("query: " + query);
+    connection.query(query, function(error, rows) {
+      if(error == null) {
+        console.log("MySQl Response: " + JSON.stringify(rows));
+        return complete({}).ok().next();
+      } else {
+        console.log(error);
+        return complete(error).runtimeError().next();
+      }
+    }) 
+  }
 
   function formatResponse(row) {
+    //TODO: LAB: format the response to match your expected JSON
     row._id = row.Id.toString();
     delete row.Id;
     row._kmd = {"ect":row.created_time, "lmt":row.last_modified_time};
