f9ff9f5a1c70e785281f4bc92ead8eeb94d291b2
Aniruddha Wani <aniruddha.w@gquotient.com>
2016-10-26 16:48:14 +0530

    used async library to break down the steps - some API methods still not implemented

--- nodepaas/partner.js
+++ nodepaas/partner.js
@@ -1,6 +1,7 @@
 var sdk = require('kinvey-backend-sdk');
 var mysql = require('mysql');
 var moment = require("moment");
+var async = require("async");
 
 var mysqlConnectionInfo = {
 	"host" : "training-mysql.cbefmmhfdivt.us-east-1.rds.amazonaws.com",
@@ -26,99 +27,9 @@ var service = sdk.service(function(err, service) {
   var dataLink = service.dataLink;
   var partner = dataLink.serviceObject('waniPartnerPaaS');
 
-  // all possible API calls
-  // onInsert	executed on inserts (or POST to REST API)
-  partner.onInsert(function(req, complete) {
-    // TODO to be tested
-    var body = mapRowKinveyToMysql(req.body);
-    var query = 'INSERT INTO partner SET ?';
-    query = mysql.format(query, body);
-    runMysqlQuery("POST", query, complete);
-  });
-  // onUpdate	executed on updates (or PUT to the REST API)
-  // onDeleteById	executed when a single entity is to be deleted
-  partner.onDeleteById(function(req, complete){
-    var query = "DELETE FROM partner WHERE Id=" + req.entityId + ";";
-    runMysqlQuery("DELETE", query, complete);
-  });
-  // onDeleteByQuery	executed when a query is included as part of a DELETE
-  // onDeleteAll	executed when the DELETE command is invoked
-  // onGetById	get a single entity by Id
-  partner.onGetById(function(req, complete){
-    var query = "SELECT * FROM partner WHERE Id=" + req.entityId + ";";
-    runMysqlQuery("GETONE", query, complete);
-  });
-  // onGetByQuery	retrieve results based on a query
-  partner.onGetByQuery(function(req, complete){
-    var query = "SELECT * FROM partner ";
-    query += parseQuery(req.query.query);
-    query += ";";
-    runMysqlQuery("GET", query, complete);
-  });
-  // onGetAll	get all entities in a given ServiceObject
-  partner.onGetAll(function(req, complete){
-    var query = "SELECT * FROM partner;";
-    runMysqlQuery("GET", query, complete);
-  });
-  // onGetCount	get the count of the entities in a ServiceObject
-  partner.onGetCount(function(req, complete){
-    var query = "SELECT COUNT(*) AS count FROM partner;";
-    runMysqlQuery("COUNT", query, complete);
-  });
-  // onGetCountByQuery	get the count of the entities in a query result
-  partner.onGetCountByQuery(function(req, complete){
-    var query = "SELECT COUNT(*) AS count FROM partner ";
-    query += parseQuery(req.query.query);
-    query += ";";
-    runMysqlQuery("COUNT", query, complete);
-  });
-
   // common functions
   function notImplementedHandler(req, complete) {
   	handleErrors(statuscodes.notImplemented,"This goes in the output as debug message!", complete);
-  }
-
-  function runMysqlQuery(method, query, complete) {
-    var connection = mysql.createConnection(mysqlConnectionInfo);
-    connection.connect(function(err) {
-      if (err) {
-        console.error('error connecting: ' + err.stack);
-        handleErrors(statuscodes.runtimeError, "Error connecting to the database", complete);
-      } else {
-        console.log('connected as id ' + connection.threadId);
-        connection.query({sql: query}, function (err, rows) {
-          if (err) {
-            console.error('error executing: ' + err.stack);
-            handleErrors(statuscodes.runtimeError, "Error running the query - " + err.code, complete);
-          } else {
-            console.log(rows);
-            // process body
-            switch(method) {
-              case "GET":
-                processGetOutput(rows, complete);
-                break;
-              case "GETONE":
-                processGetOneOutput(rows, complete);
-                break;
-              case "POST":
-                processPostOutput(rows, complete);
-                break;
-              case "PUT":
-                processPutOutput(rows, complete);
-                break;
-              case "DELETE":
-                handleSuccess(statuscodes.ok, { count : rows.affectedRows}, complete);
-                break;
-              case "COUNT":
-                handleSuccess(statuscodes.ok, { count : rows[0].count}, complete);
-                break;
-              default:
-                handleErrors(statuscodes.runtimeError, "Invalid method - highly unlikely", complete);
-            }            
-          }
-        });
-      }
-    });
   }
 
   function handleSuccess(successcode, body, complete){
@@ -158,35 +69,7 @@ var service = sdk.service(function(err, service) {
     }
   }
 
-  function parseQuery(querystring) {
-    var query = JSON.parse(querystring);
-    // TODO this is remaining
-    return "WHERE Name = 'Elon'";
-  }
-
-  function handleModifiers() {
-    // TODO this is remaining
-    return "";
-  }
-
-  function processGetOutput(rows, complete) {
-    rows.forEach(function(row) {
-      row = mapRowMysqlToKinvey(row);
-    });
-    handleSuccess(statuscodes.ok, rows, complete);
-  }
-
-  function processGetOneOutput(rows, complete) {
-    handleSuccess(statuscodes.ok, mapRowMysqlToKinvey(rows[0]), complete);
-  }
-
-  function processPostOutput(rows, complete){
-    // console.log(rows.insertId);
-    var query = "SELECT * FROM partner WHERE Id=" + rows.insertId + ";";
-    runMysqlQuery("GETONE", query, complete);
-    // handleErrors(statuscodes.notImplemented, "Not complete yet! Check logs", complete);
-  }
-
+  // synchronous functions
   function mapRowMysqlToKinvey(row) {
     row._id = row.Id;
     delete row.Id;
@@ -222,4 +105,199 @@ var service = sdk.service(function(err, service) {
     delete row._acl;
     return row;
   }
+
+  // async waterfall functions
+  function constructMySQLquery(method, req, callback) {
+    console.log(req);
+    var query = "";
+    switch(method) {
+      case "onGetCount":
+        query = "SELECT COUNT(*) AS count FROM partner;";
+        break;
+      case "onDeleteById":
+        query = "DELETE FROM partner WHERE Id=" + req.entityId + ";";
+        break;
+      case "onInsert":
+        var body = mapRowKinveyToMysql(req.body);
+        query = 'INSERT INTO partner SET ?';
+        query = mysql.format(query, body);
+        break;
+      case "onGetById":
+        query = "SELECT * FROM partner WHERE Id=" + req.entityId + ";";
+        break;
+      case "onGetAll":
+      default:
+        query+= "SELECT * FROM partner;";
+    }
+    callback(null, query);
+  }
+
+  function parseQuery(querystring) {
+    var query = JSON.parse(querystring);
+    // TODO this is remaining
+    return "WHERE Name = 'Elon'";
+  }
+
+  function handleModifiers() {
+    // TODO this is remaining
+    return "";
+  }
+
+  function establishMySQLConnection(query, callback) {
+    var connection = mysql.createConnection(mysqlConnectionInfo);
+    connection.connect(function(err) {
+      if (err) {
+        console.error('error connecting: ' + err.stack);
+        // handleErrors(statuscodes.runtimeError, "Error connecting to the database", complete);
+        callback(err);
+      }
+      console.log('connected as id ' + connection.threadId);
+      callback(null, connection, query);
+    });
+  }
+
+  function executeMySQLQuery(connection, query, callback) {
+    connection.query({sql: query}, function (err, rows) {
+      if (err) {
+        console.error('error executing: ' + err.stack);
+        // handleErrors(statuscodes.runtimeError, "Error running the query - " + err.code, complete);
+        callback(err);
+      }
+      console.log(rows);
+      // process body
+      callback(null, rows);
+    });
+  }
+
+  function processMySQLGetOutput(rows, callback){
+    rows.forEach(function(row) {
+      row = mapRowMysqlToKinvey(row);
+    });
+    callback(null, rows);
+  }
+
+  function processMySQLGetOneOutput(rows, callback) {
+    callback(null, mapRowMysqlToKinvey(rows[0]));
+  }
+
+  function processMySQLPostOutput(rows, callback){
+    // console.log(rows.insertId);
+    var query = "SELECT * FROM partner WHERE Id=" + rows.insertId + ";";
+    callback(null, query);
+    // handleErrors(statuscodes.notImplemented, "Not complete yet! Check logs", complete);
+  }
+
+  function processMySQLDeleteOuput(rows, callback) {
+    callback(null, { count : rows.affectedRows});
+  }
+
+  function processMySQLCountOutput(rows, callback) {
+    callback(null, { count : rows[0].count});
+  }
+
+  // all possible API calls
+  // onGetAll get all entities in a given ServiceObject
+  partner.onGetAll(function(req, complete){
+    async.waterfall([
+        async.apply(constructMySQLquery, "onGetAll", req),
+        establishMySQLConnection,
+        executeMySQLQuery,
+        processMySQLGetOutput
+    ], function (err, result) {
+        // result now equals 'done'
+        if (err) {
+          handleErrors(statuscodes.runtimeError, err.stack, complete);
+        } else {
+          handleSuccess(statuscodes.ok, result, complete);
+        }
+    });
+  });
+  // onGetById  get a single entity by Id
+  partner.onGetById(function(req, complete){
+    async.waterfall([
+        async.apply(constructMySQLquery, "onGetById", req),
+        establishMySQLConnection,
+        executeMySQLQuery,
+        processMySQLGetOneOutput
+    ], function (err, result) {
+        if (err) {
+          handleErrors(statuscodes.runtimeError, err.stack, complete);
+        } else {
+          handleSuccess(statuscodes.ok, result, complete);
+        }
+    });
+  });
+  // onInsert executed on inserts (or POST to REST API)
+  partner.onInsert(function(req, complete) {
+    // TODO to be tested thoroughly
+    async.waterfall([
+        async.apply(constructMySQLquery, "onInsert", req),
+        establishMySQLConnection,
+        executeMySQLQuery,
+        processMySQLPostOutput,
+        establishMySQLConnection,
+        executeMySQLQuery,
+        processMySQLGetOneOutput
+    ], function (err, result) {
+        if (err) {
+          handleErrors(statuscodes.runtimeError, err.stack, complete);
+        } else {
+          handleSuccess(statuscodes.created, result, complete);
+        }
+    });
+  });
+  // onDeleteById executed when a single entity is to be deleted
+  partner.onDeleteById(function(req, complete){
+    async.waterfall([
+        async.apply(constructMySQLquery, "onDeleteById", req),
+        establishMySQLConnection,
+        executeMySQLQuery,
+        processMySQLDeleteOuput
+    ], function (err, result) {
+        if (err) {
+          handleErrors(statuscodes.runtimeError, err.stack, complete);
+        } else {
+          handleSuccess(statuscodes.ok, result, complete);
+        }
+    });
+  });
+  // onGetCount get the count of the entities in a ServiceObject
+  partner.onGetCount(function(req, complete){
+    async.waterfall([
+        async.apply(constructMySQLquery, "onGetCount", req),
+        establishMySQLConnection,
+        executeMySQLQuery,
+        processMySQLCountOutput
+    ], function (err, result) {
+        if (err) {
+          handleErrors(statuscodes.runtimeError, err.stack, complete);
+        } else {
+          handleSuccess(statuscodes.ok, result, complete);
+        }
+    });
+  });
+
+  // TODO the ones below are remaining
+  // onUpdate executed on updates (or PUT to the REST API)
+  partner.onUpdate(notImplementedHandler);
+  // onDeleteByQuery  executed when a query is included as part of a DELETE
+  // onDeleteAll  executed when the DELETE command is invoked
+  // onGetByQuery retrieve results based on a query
+  partner.onGetByQuery(function(req, complete){
+    var query = "SELECT * FROM partner ";
+    query += parseQuery(req.query.query);
+    query += ";";
+    console.log(req);
+    // runMysqlQuery("GET", query, complete);
+    notImplementedHandler(req,complete);
+  });
+  // onGetCountByQuery  get the count of the entities in a query result
+  partner.onGetCountByQuery(function(req, complete){
+    var query = "SELECT COUNT(*) AS count FROM partner ";
+    query += parseQuery(req.query.query);
+    query += ";";
+    // runMysqlQuery("COUNT", query, complete);
+    notImplementedHandler(req,complete);
+  });
+
 });
